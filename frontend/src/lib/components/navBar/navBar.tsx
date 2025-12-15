7fff93f3695e93c6f7d7718c39f6707b6d84f2ae
Luis Felipe <luisfelipe7014feups@gmail.com>
2025-12-15 13:17:05 -0300

    feat: Ajustando CSS da NavBar

--- frontend/src/lib/components/navBar/navBar.tsx
+++ frontend/src/lib/components/navBar/navBar.tsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import  homebtn  from "../../../assets/botao-home.png"
+import  appointmentbtn  from "../../../assets/calendar.png"
+import  profilebtn  from "../../../assets/do-utilizador.png"
 
 import  "./navBar.css"
 
@@ -8,9 +11,14 @@ function NavBar(){
         <>
         <div className="navBar-container">
             <nav>
-                <Link to="/home">Home</Link>
-                <Link to="/agendamentos">Agendamentos</Link>
-                <Link to="/perfil">Perfil</Link>
+                <Link to="/home"><img id="imgs-nav" className="home-btn" 
+                src={homebtn} alt="Botao home" /></Link>
+
+                <Link to="/agendamentos"><img id="imgs-nav" className="appointment-btn" 
+                src={appointmentbtn} alt="Botao agendamentos" /></Link>
+
+                <Link to="/perfil"><img id="imgs-nav" className="profile-btn" 
+                src={profilebtn} alt="Botao perfil" /></Link>
             </nav>
         </div>
         </>
