67bab1d4d5429c061f2404bd1dcd03aae308a950
Felipe <luisfelipe7014feups@gmail.com>
2026-01-24 11:24:49 -0300

    feat/ Iniciando integracao tela de login

---
+++ frontend/src/lib/components/googleButton/googleLoginButton.tsx
@@ -0,0 +1,37 @@
+import { useGoogleLogin } from '@react-oauth/google'
+import googleButton from '../../../assets/google.png'
+
+function GoogleLoginButton() {
+
+  const login = useGoogleLogin({
+    onSuccess: tokenResponse => console.log(tokenResponse),
+    onError: error => console.log('Login Failed:', error)
+  })
+
+  return (
+    <>
+      <button onClick={() => login()}
+
+        style={{
+          backgroundColor: '#ffffffff',
+          width: 50,
+          height: 50,
+          borderRadius: 100,
+          borderStyle: 'none'
+        }}>
+        <img src={googleButton}
+          style={
+            {
+              width: 50,
+              height: 50,
+              cursor: 'pointer',
+              padding: 5
+            }
+          }
+        />
+      </button>
+    </>
+  )
+}
+
+export default GoogleLoginButton
